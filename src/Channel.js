46ec8a422b3d3a2362fcb12eb95337a35ac27c4f
Oskar Rough <oskar@rough.dk>
2021-07-01 19:05:07 +0200

    Clean up channel forms

--- src/Channel.js
+++ src/Channel.js
@@ -1,16 +1,13 @@
 import {useState, useEffect} from 'react'
 import {supabase} from './supabaseClient'
 
+// <Account> tries to fetch user's channel.
+//	has channel? <EditForm channel>
+//	else <CreateForm></CreateForm>
+
 export default function Account({session}) {
 	const [loading, setLoading] = useState(false)
-	const [deleting, setDeleting] = useState(false)
-	const [name, setName] = useState('')
-	const [slug, setSlug] = useState('')
-	const [channel, setChannel] = useState('')
-
-	useEffect(() => {
-		findChannel()
-	}, [session])
+	const [channel, setChannel] = useState(false)
 
 	async function findChannel() {
 		try {
@@ -22,14 +19,42 @@ export default function Account({session}) {
 				.eq('user_id', user.id)
 				.single()
 			if (error && status !== 406) throw error
-			console.log('found channel', data)
 			if (data) setChannel(data)
-			if (!data) setChannel(null)
 		} catch (error) {
 			console.error(error.message)
 		} finally {
 			setLoading(false)
 		}
+	}
+
+	// Start fetching channel as soon as we have a session.
+	useEffect(() => {
+		findChannel()
+	}, [session])
+
+	console.log('render', channel)
+
+	return (
+		<div>
+			If you have a radio, show the edit form.
+			<br />
+			If you don't, show the create form
+			{!loading && channel?.id ? (
+				<EditForm channel={channel} onDelete={setChannel}></EditForm>
+			) : (
+				<CreateForm onCreate={setChannel}></CreateForm>
+			)}
+		</div>
+	)
+}
+
+function CreateForm({onCreate}) {
+	const [loading, setLoading] = useState(false)
+	const [form, setForm] = useState({})
+
+	function onSubmit(e) {
+		e.preventDefault()
+		createChannel({name: form.name, slug: form.slug})
 	}
 
 	async function createChannel({name, slug}) {
@@ -39,8 +64,10 @@ export default function Account({session}) {
 			let {data, error} = await supabase
 				.from('channels')
 				.insert({name, slug, user_id: user.id, updated_at: new Date()})
+				.single()
 			if (error) throw error
-			setChannel(data)
+			console.log(data)
+			onCreate(data)
 		} catch (error) {
 			console.error(error.message)
 		} finally {
@@ -48,12 +75,53 @@ export default function Account({session}) {
 		}
 	}
 
-	async function updateChannel({name, slug}) {
+	return (
+		<form onSubmit={onSubmit}>
+			<h3>Create form</h3>
+			<p>
+				<label htmlFor="name">What would you like to call your radio channel?</label>
+				<br />
+				<input
+					id="name"
+					name="name"
+					// value={form.name}
+					onChange={(e) => setForm({...form, [e.target.name]: e.target.value})}
+					required
+				/>
+				<br />
+				<label htmlFor="slug">And the URL? (e.g. radio4000.com/{form.slug})</label>
+				<br />
+				<input
+					id="slug"
+					name="slug"
+					// value={form.slug}
+					onChange={(e) => setForm({...form, [e.target.name]: e.target.value})}
+					required
+				/>
+			</p>
+			<p>
+				<button type="submit" disabled={loading}>
+					{loading ? 'Loading...' : 'Make it so'}
+				</button>
+			</p>
+		</form>
+	)
+}
+
+function EditForm({channel, onDelete}) {
+	const [loading, setLoading] = useState(false)
+	const [deleting, setDeleting] = useState(false)
+	const [form, setForm] = useState(channel)
+
+	function onSubmit(event) {
+		event.preventDefault()
+		updateChannel(form)
+	}
+
+	async function updateChannel({id, name, slug}) {
 		try {
 			setLoading(true)
-			let {error} = await supabase
-				.from('channels')
-				.upsert({id: channel.id, name, slug, updated_at: new Date()})
+			let {error} = await supabase.from('channels').upsert({id, name, slug, updated_at: new Date()})
 			if (error) throw error
 		} catch (error) {
 			console.error(error.message)
@@ -63,83 +131,64 @@ export default function Account({session}) {
 	}
 
 	async function deleteChannel(id) {
+		if (!channel.id) return
 		try {
 			setDeleting(true)
 			let {error} = await supabase.from('channels').delete().match({id})
 			if (error) throw error
+			console.log('deleted channel')
+			onDelete()
 		} catch (error) {
 			console.error(error.message)
 		} finally {
 			setDeleting(false)
-			await findChannel()
 		}
 	}
 
 	function confirmDelete() {
-		console.log(channel.id)
-		if (!channel?.id) return
 		const didConfirm = window.confirm('Confirm you want to delete your channel')
 		didConfirm && deleteChannel(channel.id)
 	}
 
-	console.log({channel})
-
-	if (channel?.id)
-		return (
-			<div>
-				<p>
-					Your channels name is <strong>{channel.name}</strong>
-				</p>
-			<form
-				onSubmit={(e) => {
-					e.preventDefault()
-					updateChannel({name, slug})
-				}}
-			>
-				<p>
-					<label htmlFor="name">Name</label>
-					<input id="name" value={name} onChange={(e) => setName(e.target.value)} required />
-					<br />
-
-					<label htmlFor="slug">Slug</label>
-					<input id="slug" value={slug} onChange={(e) => setSlug(e.target.value)} required />
-				</p>
-				<p>
-					<button type="submit" disabled={loading}>
-						{loading ? 'Loading...' : 'Update'}
-					</button>
-				</p>
-			</form>
-				<p>
-					<button type="button" onClick={confirmDelete}>
-						{deleting ? 'Deleting...' : 'Delete'}
-					</button>
-				</p>
-			</div>
-		)
-
 	return (
-		<div>
-			<form
-				onSubmit={(e) => {
-					e.preventDefault()
-					createChannel({name, slug})
-				}}
-			>
-				<p>
-					<label htmlFor="name">What would you like to call your radio channel?</label><br />
-					<input id="name" value={name} onChange={(e) => setName(e.target.value)} required />
-					<br />
-
-					<label htmlFor="slug">And the URL? (e.g. radio4000.com/{slug})</label><br/>
-					<input id="slug" value={slug} onChange={(e) => setSlug(e.target.value)} required />
-				</p>
-				<p>
-					<button type="submit" disabled={loading}>
-						{loading ? 'Loading...' : 'Make it so'}
-					</button>
-				</p>
-			</form>
-		</div>
+		<form onSubmit={onSubmit}>
+			<h3>Edit form</h3>
+			<p>
+				name: {channel.name}
+				<br />
+				slug: {channel.slug}
+			</p>
+			<p>
+				<label htmlFor="name">Name</label>
+				<input
+					id="name"
+					name="name"
+					placeholder={channel.name}
+					value={form.name}
+					onChange={(e) => setForm({...form, [e.target.name]: e.target.value})}
+					required
+				/>
+				<br />
+				<label htmlFor="slug">Slug</label>
+				<input
+					id="slug"
+					name="slug"
+					placeholder={channel.slug}
+					value={form.slug}
+					onChange={(e) => setForm({...form, [e.target.name]: e.target.value})}
+					required
+				/>
+			</p>
+			<p>
+				<button type="submit" disabled={loading}>
+					{loading ? 'Loading...' : 'Update'}
+				</button>
+			</p>
+			<p>
+				<button type="button" onClick={confirmDelete}>
+					{deleting ? 'Deleting...' : 'Delete'}
+				</button>
+			</p>
+		</form>
 	)
 }
