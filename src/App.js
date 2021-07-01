883b996794d93f1fdb77a1f047d92849098579f3
Oskar Rough <oskar@rough.dk>
2021-07-01 16:08:31 +0200

    Lint

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Switch, Route, Link, useLocation} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 import {useState, useEffect} from 'react'
 import {supabase} from './supabaseClient'
 import Auth from './Auth'
