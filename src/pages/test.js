05a68e0dfeccb1fc201b7db895f990039c209111
Oskar Rough <oskar@rough.dk>
2021-07-03 13:30:00 +0200

    Set default fetcher and pass dynamic query!

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,20 +1,11 @@
 import useSWR from 'swr'
 import {supabase} from '../utils/supabaseClient'
 
-// @todo somehow pass this query in through useSWR to the fetcher.
+// This CAN NOT be inside the React component.
 const QUERY = supabase.from('channels').select('*')
 
-// We overwrite the default fetcher because supabase returns {data, error},
-// which we need to handle.
-// SWR expects either some data or to throw an error.
-async function supabaseFetcher() {
-	const {data, error} = await QUERY
-	if (error) throw new Error(error)
-	return data
-}
-
 export default function Test() {
-	const {data, error} = useSWR('a unique identifier for caching purposes', supabaseFetcher)
+	const {data, error} = useSWR(['a unique identifier', QUERY])
 
 	if (error) return <div>Error</div>
 	if (!data) return <div>Loading...</div>
--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -2,6 +2,15 @@
 
 // This is our global fetcher used for the SWR hook.
 // If you want to override it, pass another "fetcher" as the second argument to useSWR()
-const fetcher = (...args) => fetch(...args).then((res) => res.json())
+// const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
-export default fetcher
+// We overwrite the default fetcher because supabase returns {data, error},
+// which we need to handle.  SWR expects either some data or to throw an error.
+const supabaseFetcher = async function supabaseFetcher(...args) {
+	const {data, error} = await args[1]
+	if (error) throw new Error(error)
+	return data
+}
+
+
+export default supabaseFetcher
