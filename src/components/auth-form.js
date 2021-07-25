26c130e877416dccae29b1eb7841199cb27eccb6
Oskar Rough <oskar@rough.dk>
2021-07-25 11:01:52 +0200

    Add status messages to the UI

--- src/components/auth-form.js
+++ src/components/auth-form.js
@@ -4,29 +4,10 @@ export default function Auth({
 	onSubmit
 }) {
 	const [loading, setLoading] = useState(false)
-	const [data, setData] = useState({
-		email: '',
-		password: ''
-	})
+	const [message, setMessage] = useState(false)
+	const [errorMessage, setErrorMessage] = useState(false)
+	const [data, setData] = useState({email: '', password: ''})
 	const {email, password} = data
-
-	const handleLogin = async (loginData) => {
-		try {
-			setLoading(true)
-			const {error} = await onSubmit(loginData)
-			if (error) throw error
-			if (!password) {
-				console.info('Check your email for the login link!')
-			}
-		} catch (error) {
-			console.log(error)
-		}
-	}
-
-	const handleSubmit = (e) => {
-		e.preventDefault()
-		handleLogin(data)
-	}
 
 	const handleChange = ({target}) => {
 		const {name, value} = target
@@ -36,31 +17,57 @@ export default function Auth({
 		})
 	}
 
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		handleLogin(data)
+	}
+
+	const handleLogin = async (loginData) => {
+		try {
+			setLoading(true)
+			const {error} = await onSubmit(loginData)
+			if (error) {
+				setErrorMessage(error)
+				throw error
+			}
+			setErrorMessage(false)
+			if (!password) {
+				setMessage('Check your email for the login link!')
+			}
+		} catch (error) {
+			console.log(error)
+			setErrorMessage(error)
+		} finally {
+			setLoading(false)
+		}
+	}
+
 	return (
 		<div>
 			<p>Sign in via magic link with your email below</p>
-			<form
-				onSubmit={handleSubmit}
-			>
+			<form onSubmit={handleSubmit}>
 				<input
-				name="email"
-				type="email"
-				placeholder="Your email"
-				autoFocus={true}
-				value={email}
-				onChange={handleChange}
+					name="email"
+					type="email"
+					placeholder="Your email"
+					autoFocus={true}
+					value={email}
+					onChange={handleChange}
+					required
 				/>
 				<input
-				name="password"
-				type="password"
-				placeholder="Your password"
-				value={password}
-				onChange={handleChange}
+					name="password"
+					type="password"
+					placeholder="Your password"
+					value={password}
+					onChange={handleChange}
 				/>
 				<button disabled={loading}>
 					{loading ? <span>Loading</span> : <span>Send magic link</span>}
 				</button>
 			</form>
+			{errorMessage && <p danger="true">Error: {errorMessage.message}</p>}
+			{message && <p>{message}</p>}
 		</div>
 	)
 }
