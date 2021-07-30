3e28d5dd47acadb22fc5f7990548ec38bf2dd435
Oskar Rough <oskar@rough.dk>
2021-07-30 13:54:39 +0200

    Fix /channels page for unauthed users

--- src/pages/channels.js
+++ src/pages/channels.js
@@ -1,12 +1,12 @@
 import useChannels from '../hooks/use-channels'
 import Tracks, {CreateTrackForm} from '../components/tracks'
 
-const PageChannels = ({dbSession: {database, session}}) => {
+export default function PageChannels({dbSession: {database, session}}) {
 	const channels = useChannels(database)
 
-	if (!database) return null
-
+	// if (!database || !session) return <p>Hum this should not happen</p>
 	if (!channels.length) return <p>No channels</p>
+
 	return channels.map((channel) => {
 		return (
 			<article key={channel.id}>
@@ -14,17 +14,19 @@ const PageChannels = ({dbSession: {database, session}}) => {
 					<span>{channel.name}</span> <i>@{channel.slug}</i>
 				</h2>
 				<hr />
-				<h3>Add track</h3>
-				<CreateTrackForm
-					channelId={channel.id}
-					database={database}
-					userId={session.user.id}
-				></CreateTrackForm>
-				<hr />
+				{session && (
+					<>
+						<h3>Add track</h3>
+						<CreateTrackForm
+							channelId={channel.id}
+							database={database}
+							userId={session.user.id}
+						></CreateTrackForm>
+						<hr />
+					</>
+				)}
 				<Tracks channelId={channel.id} database={database}></Tracks>
 			</article>
 		)
 	})
 }
-
-export default PageChannels
