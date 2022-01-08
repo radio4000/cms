6813fc025919607bfe2cf1b111dac09a443b2dd5
hugur <hugu.r.p+hello@pm.me>
2022-01-08 16:50:57 +0100

    improve user channel select

--- src/components/channels/channels.js
+++ src/components/channels/channels.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link, NavLink} from 'react-router-dom'
 
 export default function Channels({channels}) {
@@ -13,18 +13,32 @@ export default function Channels({channels}) {
 	) : <p>No channels</p>
 }
 
+/*
+	 A component to select from the user's channels,
+	 and handle a choice
+ */
 export const UserChannelsSelect = ({
 	userChannel, userChannels, onChange
 }) => {
+	const [isActive, setIsActive] = useState()
 	if (!userChannel || userChannel === null) return null
+
+	/*
+		 needs <select value=""> empty,
+		 to it always reset the select to the default option (current channel),
+		 and all other option are clickable (because non of them is the "select's value")
+*/
 	return (
 		userChannel && (userChannels?.length > 1) ? (
 			<select
+				value=""
 				name={userChannel.slug}
-				value={userChannel.slug}
 				onChange={onChange}
 			>
-				<option disabled={true}
+				<option
+					defaultValue={true}
+					value=""
+					disabled={true}
 				>@{userChannel.name}</option>
 				{userChannels.map(channel => (
 					<option
