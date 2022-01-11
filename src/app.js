2154a42639a0818f0efe8b2b34a566e713bff829
Oskar Rough <oskar@rough.dk>
2022-01-11 13:19:55 +0100

    Refactor auth routes

--- src/app.js
+++ src/app.js
@@ -7,9 +7,9 @@ import DbSession from 'components/db-session'
 import Layout from 'layouts/app'
 import PageChannels from 'pages/channels'
 import PageChannel from 'pages/channel'
-import PageRegister from 'pages/register'
-import PageLogin from 'pages/login'
-import PageLogout from 'pages/logout'
+import PageRegister from 'pages/auth/register'
+import PageLogin from 'pages/auth/login'
+import PageLogout from 'pages/auth/logout'
 import PageSettingsAccount from 'pages/account'
 import PageSettingsChannels from 'pages/account/channels'
 import PageNewChannel from 'pages/new'
@@ -18,7 +18,7 @@ import PageHome from 'pages/home'
 import PageNoMatch from 'pages/404'
 import PageTest from 'pages/test'
 import PageAdd from 'pages/add'
-import PageResetPassword from 'pages/reset-password'
+import PageResetPassword from 'pages/auth/reset-password'
 import PageChannelEdit from 'pages/channel/edit'
 
 import AuthRequired from 'components/auth-required'
@@ -49,19 +49,14 @@ export default function App() {
 										</AuthRequired>
 									}></Route>
 
-									<Route
-									path="reset-password"
-									element={<PageResetPassword dbSession={dbSession} />}
-									/>
+									<Route path="reset-password" element={<PageResetPassword dbSession={dbSession} />} />
+
 									{/* Channel(s) */}
-									<Route path="new" element={<PageNewChannel dbSession={dbSession} />}></Route>
+									<Route path="new" element={<PageNewChannel dbSession={dbSession} />} />
 									<Route path="new/import" element={<PageNewChannelImport dbSession={dbSession} />} />
 									<Route path="channels" element={<PageChannels dbSession={dbSession} />} />
-									<Route path=":slug" element={<PageChannel dbSession={dbSession} />}></Route>
-									<Route
-									path=":slug/edit"
-									element={<PageChannelEdit dbSession={dbSession} />}
-									/>
+									<Route path=":slug" element={<PageChannel dbSession={dbSession} />} />
+									<Route path=":slug/edit" element={<PageChannelEdit dbSession={dbSession} />} />
 									<Route path="add" element={<PageAdd dbSession={dbSession} />} />
 									<Route path="add/:slug" element={<PageAdd dbSession={dbSession} />} />
 
--- src/components/auth-required.js
+++ src/components/auth-required.js
@@ -4,11 +4,7 @@ export default function AuthRequired({session, children}) {
 	if (!session) {
 		return (
 			<p>
-				<Link to={{
-					pathname: `/login`
-				}}>Login</Link>
-				{' '}
-				to view this page.
+				<Link to={{pathname: `/login`}}>Login</Link> to view this page.
 			</p>
 		)
 	}
