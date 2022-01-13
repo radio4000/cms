9cf63e0bee5e61e6c998a7d7aae78e3bc0f9a7ab
Oskar Rough <oskar@rough.dk>
2022-01-13 20:43:58 +0100

    Remove channels by track activity on homepage until it works

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,42 +5,31 @@ import Channels from 'components/channels'
 
 const {RADIO4000_APP_NAME} = config
 
-const sortByUpdated = (a, b) => (a['updated_at'] > b['updated_at'] ? 1 : -1)
 
 export default function PageHome({dbSession: {database, session, userChannel}}) {
 	const {channels} = useChannels(database)
 	const channelsLimit = 3
-	const channelsLastUpdate = [...channels].sort(sortByUpdated).slice(0, channelsLimit)
 	const channelsLastCreated = channels.slice(0, channelsLimit)
 
 	return (
 		<>
-			{!session ? (
+			{!session && (
 				<header>
 					<p>Welcome to the new {RADIO4000_APP_NAME}!</p>
 				</header>
-			) : (
-				<>
-					{userChannel && (
-						<p>
-							Go to your channel <Link to={`/${userChannel.slug}/`}>{userChannel.slug}</Link>.
-						</p>
-					)}
-				</>
 			)}
 
-			{channelsLastUpdate && (
-				<>
-					<p>Recent activity</p>
-					<Channels channels={channelsLastUpdate} />
-				</>
+			{userChannel && (
+				<p>
+					Go to your channel <Link to={`/${userChannel.slug}/`}>{userChannel.slug}</Link>.
+				</p>
 			)}
 
 			{channelsLastCreated && (
-				<>
-					<p>New radios</p>
+				<section>
+					<h3>Latest channels</h3>
 					<Channels channels={channelsLastCreated} />
-				</>
+				</section>
 			)}
 		</>
 	)
