aa1396564888dc416ae47f7e4f876c6a6d56504a
Oskar Rough <oskar@rough.dk>
2022-01-30 22:47:44 +0100

    Add fake play buttons to channel and track lists

--- src/components/tracks/tracks.js
+++ src/components/tracks/tracks.js
@@ -1,18 +1,22 @@
+import {usePlayer} from 'contexts/player'
 import Track from './track'
 
 export default function Tracks({database, tracks, canEdit, afterDelete}) {
+	const player = usePlayer()
 	if (!tracks?.length) return <p>No tracks</p>
 
 	return (
 		<section className="Tracks">
 			{tracks.map((track) => (
-				<Track
-					key={track.id}
-					database={database}
-					track={track}
-					canEdit={canEdit}
-					afterDelete={afterDelete}
-				></Track>
+				<div key={track.id}>
+					<button type="button" onClick={() => player.setTrack(track)}>▶</button>
+					<Track
+						database={database}
+						track={track}
+						canEdit={canEdit}
+						afterDelete={afterDelete}
+					></Track>
+				</div>
 			))}
 		</section>
 	)
